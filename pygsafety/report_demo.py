576c84a2f374f77bcef1da870dcec70b77645b92
alexander <zhe009@hotmail.com>
2017-09-17 21:33:06 +0800

    update

--- pygsafety/report_demo.py
+++ pygsafety/report_demo.py
@@ -7,23 +7,30 @@ from report_lib import Reporter
 host_work = 'http://192.168.29.31:7003'
 
 reporter = Reporter(host_work)
-reporter.login('username', 'password')
+# reporter.login('username', 'password')
 
-# reporter.printHistory('2017-05-15', '2017-05-21')
-# print('===== before =====')
-
-reporter.logWorklog(date='2017-06-26', time_start= '09:00', time_end= '18:00', time_o_start= '20:30', time_o_end= '21:30', content='''
-    Jenkins构建队列云平台环境调试
-    一张图项目Sonarqube代码扫描规则评审并给项目负责人讲解
-    ''')
-reporter.logWorklog(date='2017-06-27', time_start= '09:00', time_end= '18:00', time_o_start= '18:00', time_o_end= '21:20', content='云平台项目构建中遇到的依赖缺失故障的排查与修复')
-reporter.logWorklog(date='2017-06-21', time_start= '09:00', time_end= '18:00', time_o_start= '18:00', time_o_end= '21:00', content='Jenkins构建队列调度服务设计与实现')
-reporter.logWorklog(date='2017-06-22', time_start= '09:00', time_end= '18:00', time_o_start= '18:00', time_o_end= '22:30', content='Jenkins构建队列调度动态同步设计与实现')
-reporter.logWorklog(date='2017-06-23', time_start= '09:00', time_end= '18:00', time_o_start= '18:00', time_o_end= '21:20', content='Jenkins构建脚本动态多模块功能调试及实现')
-reporter.logWorklog(date='2017-06-24', time_start= '',      time_end= '',      time_o_start= '11:00', time_o_end= '18:20', content='软件版本发布流程设计')
-reporter.logWorklog(date='2017-06-25', time_start= '',      time_end= '',      time_o_start= '10:00', time_o_end= '20:00', content='nginx调优及在线文档路径设计')
-
+print('===== before =====')
+reporter.log_worklogs(
+    log_content='''
+    你周一干了啥
+    你周二干了啥
+    你周三干了啥
+    你周四干了啥
+    你周五干了啥
+    你周六干了啥
+    你周七干了啥
+    ''',
+    log_datetime='''
+    20170911 09:00-18:00 18:00-21:50
+    20170912 09:00-18:00 18:00-21:30
+    20170913 09:00-18:00 18:00-22:00
+    20170914 09:00-18:00 18:00-21:30
+    20170915 09:00-18:00
+    20170916  18:00-21:30
+    20170917  11:30-21:00
+    '''
+)
 print('===== after ======')
-reporter.printHistory('2017-06-19', '2017-06-25')
+reporter.print_history('2017-09-11', '2017-09-17')
 
 # http://192.168.29.31:7003/ams/ams_weekly/WeeklyweeklyEdit.do?weeklyweeklyid=40289d9f5981549c015c49b1a8f66998
--- pygsafety/report_lib.py
+++ pygsafety/report_lib.py
@@ -33,7 +33,7 @@ class Reporter:
         )
         # print(resp)
 
-    def getHistory(self, date_start, date_end = None):
+    def query_history(self, date_start, date_end = None):
         data = urlencode({
             'projectid':'',
             'formid':'frmSearch',
@@ -133,15 +133,15 @@ class Reporter:
 
         return data
 
-    def printHistory(self, date_start, date_end = None):
-        data = self.getHistory(date_start, date_end)
+    def print_history(self, date_start, date_end = None):
+        data = self.query_history(date_start, date_end)
         
         data['reports'] = sorted(data['reports'], key=lambda x: x['days'][0])
 
         for report in data['reports']:
-            print('=' * 128)
-            print('%36s : %s ' % (report['param'], report['project']))
+            # print('=' * 128)
             print('-' * 128)
+            print('%36s : %s ' % (report['param'], report['project']))
 
             contents = report['content'].strip().splitlines()
             days = []
@@ -166,7 +166,7 @@ class Reporter:
                 print('%36s : %s ' % (days[i], contents[i]))
             print('')
 
-    def logWorklog(self, content, date, time_start='', time_end='', time_o_start='', time_o_end=''):
+    def log_worklog(self, content, date, time_start='', time_end='', time_o_start='', time_o_end=''):
         data = urlencode({
             'projectid':'2c90827052e7b61401535a546c0e0609',
             'formid':'frmCreate',
@@ -203,3 +203,28 @@ class Reporter:
         )
         print(resp)
         # print(content)
+
+    def log_worklogs(self, log_content, log_datetime=None):
+        content_lines = [line.strip() for line in log_content.strip().splitlines()]
+        if log_datetime:
+            datetime_lines = [line.strip() for line in log_datetime.strip().splitlines()]
+
+            for index in range(0, len(content_lines)):
+                # print(datetime_lines[index])
+                # print(content_lines[index])
+                datetime_parts = datetime_lines[index].split(' ') + ['']
+
+                # print('%8s %11s %11s %s' % (datetime_parts[0], datetime_parts[1], datetime_parts[2], content_lines[index]))
+                # print('%s-%s-%s' % (datetime_parts[0][0:4], datetime_parts[0][4:6], datetime_parts[0][6:8]))
+                # print(datetime_parts[1][0:5])
+                # print(datetime_parts[1][6:11])
+                # print(datetime_parts[2][0:5])
+                # print(datetime_parts[2][6:11])
+                self.log_worklog(
+                    date='%s-%s-%s' % (datetime_parts[0][0:4], datetime_parts[0][4:6], datetime_parts[0][6:8]),
+                    time_start=datetime_parts[1][0:5],
+                    time_end=datetime_parts[1][6:11],
+                    time_o_start=datetime_parts[2][0:5],
+                    time_o_end=datetime_parts[2][6:11],
+                    content=content_lines[index]
+                )
