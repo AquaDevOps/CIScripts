09cb0feac25251c3758486ceeda91fc284e38394
xuwenzhe <xuwenzhe@gsafety.com>
2018-01-03 14:56:07 +0800

    init with simple package structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='aqua-devops',
+    version='0.0.1-20180103',
+    description='lib for devops',
+    packages=find_packages(),
+    license='Apache 2.0',
+    url='https://github.com/popingalex',
+    author='popingalex',
+    author_email='zhe009@hotmail.com',
+)
