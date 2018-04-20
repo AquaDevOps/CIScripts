e391e39ecebab8794e7389199ff34bfbbaba7f86
xuwenzhe <xuwenzhe@gsafety.com>
2018-04-20 09:59:15 +0800

    [DEVOPS-389] split cn_check from cn_format

--- devops/utils/string.py
+++ devops/utils/string.py
@@ -1,8 +1,11 @@
-def ch(string, offset=0):
+def cn_format(string, offset=0):
     if offset:
-        count_ch = len([s for s in string if u'\u4e00' <= s <= u'\u9fa5'])
+        count_ch = len([s for s in string if cn_check(s)])
         if offset > 0:
             return string + (abs(offset) - count_ch - len(string)) * ' '
         else:
             return (abs(offset) - count_ch - len(string)) * ' ' + string
     return string
+
+def cn_check(s):
+    return u'\u4e00' <= s <= u'\u9fa5'
