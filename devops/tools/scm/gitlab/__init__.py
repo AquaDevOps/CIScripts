939abaa23221fc3d930ac1a67deb252b3e954450
lipengfei <lipengfei@gsafety.com>
2018-04-23 17:18:37 +0800

    modify

--- devops/tools/scm/gitlab/__init__.py
+++ devops/tools/scm/gitlab/__init__.py
@@ -18,39 +18,40 @@ class Gitlab:
         self.group = GroupHelper(self)
         self.user = UserHelpder(self)
 
-    def create(self, owner, project_number, template, display_name, members=[]):
+    def create(self, owner, project_number, template, project_name, members):
         self.group.create(path=project_number, name=None)
         groupid = self.group.list(project_number)[0]['id']
 
         ownerid = self.user.list(search={'username': owner})[0]['id']
         self.group.add_member(userid=ownerid, access_level=50, groupid=groupid)
-        name = display_name
+        name = project_name
         self.project.create(owner=ownerid, name=name, groupid=groupid, path=name)
         project = self.project.list(project_number+'/'+name)[0]['id']
-        if members:
-            for member in members:
-                member = self.user.list(search={'username': member})[0]['id']
-                try:
-                    self.project.add_member(userid=member, access_level=30, projectid=project)
-                except Exception as e:
-                    pass
-        self.add_authz(display_name, project_number, members)
-
-    def add_authz(self, display_name, project_number, members=[]):
-        # add ldap
-        groups = get_members(project_number)
-        name = display_name
-        project = self.project.list(project_number+'/'+name)[0]['id']
-        for r in groups.keys():
-            for member in groups[r]:
+        for r in members.keys():
+            for member in members[r]:
                 member = self.user.list(search={'username': member})[0]['id']
                 try:
                     self.project.add_member(userid=member, access_level=ROLE_2_LEVEL[r], projectid=project)
                 except Exception as e:
                     pass
+        self.add_authz(project_name, project_number, members)
 
-    def get_auth(self, project_number, display_name):
-        name = display_name
+    def add_authz(self, project_name, project_number, members):
+
+        if self.auth_ldap(project_number, project_name):
+            name = project_name
+            project = self.project.list(project_number+'/'+name)[0]['id']
+            for r in members.keys():
+                for member in members[r]:
+                    member = self.user.list(search={'username': member})[0]['id']
+                    try:
+                        self.project.add_member(userid=member, access_level=ROLE_2_LEVEL[r], projectid=project)
+                    except Exception as e:
+                        pass
+        # add ldap
+
+    def get_auth(self, project_number, project_name):
+        name = project_name
         project = self.project.list(project_number+'/'+name)[0]['id']
         members = self.project.get_members(project)
         field={}
@@ -62,9 +63,9 @@ class Gitlab:
             field[LEVEL_2_ROLE[level]].append(m['username'])
         return field
 
-    def modify_ldap(self, project_number, display_name):
+    def auth_ldap(self, project_number, project_name):
         if self.get_auth(project_number=project_number,
-                         display_name=display_name) == get_members(project_number, display_name):
+                         project_name=project_name) == get_members(project_number, project_name):
             return True
         else:
             return False
--- devops/tools/scm/svn/__init__.py
+++ devops/tools/scm/svn/__init__.py
@@ -19,9 +19,9 @@ class svn:
 
         self.template = template.catalog
 
-    def create(self, owner, project_number, template, display_name, members=[]):
+    def create(self, owner, project_number, template, project_name, members):
 
-        display_name = self.svn_repo_name
+        # project_name = self.svn_repo_name
         create_command = []
         command1 = 'mkdir {svn_home}/{project_number}'
         comm1 = command1.format(
@@ -34,13 +34,13 @@ class svn:
         comm2 = command2.format(
             svn_home=self.svn_home,
             project_number=project_number,
-            doc=display_name
+            doc=project_name
         )
         create_command.append(comm2)
 
         command3 = 'svn mkdir file://{svn_home}/{project_number}/{doc}/{init}  -m "initial directories"'
         comm3 = command3.format(
-            doc=display_name,
+            doc=project_name,
             svn_home=self.svn_home,
             project_number=project_number,
             init=self.template[template] if self.template.__contains__(template) else "{tags,trunk,branches}"
@@ -56,7 +56,7 @@ class svn:
 
   AuthType Basic
   AuthName "Repositories of {project_number}"
-  AuthzSVNAccessFile {svn_home}/{project_number}/{display_name}/authz
+  AuthzSVNAccessFile {svn_home}/{project_number}/{project_name}/authz
 
   Satisfy all
   Require valid-user
@@ -69,7 +69,7 @@ class svn:
         repo_conf = repo_conf.format(
             project_number=project_number,
             svn_home=self.svn_home,
-            display_name=display_name
+            project_name=project_name
         )
         command4 = "echo '{repo_conf}' >>/etc/httpd/conf.d/subversion.conf"
         comm4 = command4.format(
@@ -77,51 +77,14 @@ class svn:
         )
         create_command.append(comm4)
 
-        repo_authz = """[groups]
-developer = {user3}
-
-[{doc}:/]
-* = r
-
-[{doc}:/*]
-@developer = rw"""
-        users = owner
-        for user in members:
-            users = users + ',' + user
-        repo_authz = repo_authz.format(
-            user3=users,
-            doc=display_name
-        )
-        command5 = 'echo "{repo_authz}" >>{svn_home}/{project_number}/{display_name}/authz'
-        comm5 = command5.format(
-            project_number=project_number,
-            svn_home=self.svn_home,
-            repo_authz=repo_authz,
-            display_name=display_name
-        )
-        create_command.append(comm5)
-
-        comm6 = "systemctl restart httpd.service"
-        create_command.append(comm6)
-        try:
-            for c in create_command:
-                self.client.exec_command(c)
-        except Exception as e:
-            pass
-        self.add_authz(display_name, project_number, members)
-
-    def add_authz(self, display_name, project_number, members=[]):
-        # add ldap
-
-        groups = get_members(project_number, display_name)
         repo_groups = '[groups]'
-        for r in groups.keys():
+        for r in members.keys():
             repo_groups = repo_groups + """
 """ + r + "="
-            members = ','.join(groups[r])
-            repo_groups = repo_groups + members
-        repo_field = "[" + display_name + ":/]"
-        for ff in groups.keys():
+            r_members = ','.join(members[r])
+            repo_groups = repo_groups + r_members
+        repo_field = "[" + project_name + ":/]"
+        for ff in members.keys():
             repo_field = repo_field + """
 @""" + ff + " = rw"
 
@@ -132,15 +95,12 @@ developer = {user3}
             groups=repo_groups,
             field=repo_field
         )
-        # print(repo_auth)
-
-        create_command = None
-        command5 = 'echo "{repo_authz}" >{svn_home}/{project_number}/{display_name}/authz'
+        command5 = 'echo "{repo_authz}" >>{svn_home}/{project_number}/{project_name}/authz'
         comm5 = command5.format(
             project_number=project_number,
             svn_home=self.svn_home,
             repo_authz=repo_auth,
-            display_name=display_name
+            project_name=project_name
         )
         create_command.append(comm5)
 
@@ -151,13 +111,56 @@ developer = {user3}
                 self.client.exec_command(c)
         except Exception as e:
             pass
+        # self.add_authz(project_name, project_number, members)
 
-    def get_auth(self, project_number, display_name):
-        c = 'cat {svn_home}/{project_number}/{display_name}/authz'
+    def add_authz(self, project_name, project_number, new_members):
+        # add ldap
+
+        if self.auth_ldap(project_number, project_name):
+            repo_groups = '[groups]'
+            for r in new_members.keys():
+                repo_groups = repo_groups + """
+""" + r + "="
+                members = ','.join(new_members[r])
+                repo_groups = repo_groups + members
+            repo_field = "[" + project_name + ":/]"
+            for ff in new_members.keys():
+                repo_field = repo_field + """
+@""" + ff + " = rw"
+
+            repo_authz = """{groups}
+    
+{field}"""
+            repo_auth = repo_authz.format(
+                groups=repo_groups,
+                field=repo_field
+            )
+            # print(repo_auth)
+
+            create_command = None
+            command5 = 'echo "{repo_authz}" >{svn_home}/{project_number}/{project_name}/authz'
+            comm5 = command5.format(
+                project_number=project_number,
+                svn_home=self.svn_home,
+                repo_authz=repo_auth,
+                project_name=project_name
+            )
+            create_command.append(comm5)
+
+            comm6 = "systemctl restart httpd.service"
+            create_command.append(comm6)
+            try:
+                for c in create_command:
+                    self.client.exec_command(c)
+            except Exception as e:
+                pass
+
+    def get_auth(self, project_number, project_name):
+        c = 'cat {svn_home}/{project_number}/{project_name}/authz'
         c = c.format(
             svn_home=self.svn_home,
             project_number=project_number,
-            display_name=display_name
+            project_name=project_name
         )
         stdin, stdout, stderr = self.client.exec_command(c)
 
@@ -185,9 +188,9 @@ developer = {user3}
             print(value)
             return value
 
-    def modify_ldap(self, project_number, display_name):
+    def auth_ldap(self, project_number, project_name):
         if self.get_auth(project_number=project_number,
-                         display_name=display_name) == get_members(project_number, display_name):
+                         project_name=project_name) == get_members(project_number, project_name):
             return True
         else:
             return False
