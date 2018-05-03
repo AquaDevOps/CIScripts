2d3a3c228d30cdffc63d14bd73fedf65bef0eade
xuwenzhe <xuwenzhe@gsafety.com>
2018-05-03 19:28:19 +0800

    add database synchronizer

--- devops/tools/__init__.py
+++ devops/tools/__init__.py
@@ -2,7 +2,7 @@ from . import atlassian
 from . import builder
 from . import ci
 from . import database
-from . import ldap as ldap
-from . import nginx
-from . import office
-from . import scm
+# from . import ldap as ldap
+# from . import nginx
+# from . import office
+# from . import scm
